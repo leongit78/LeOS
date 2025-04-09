e2883703c30ae06dea9d9400380e92ad3d85f995
leongit78 <leonidivanov993@gmail.com>
2025-04-10 02:23:39 +0300

    Added print_hex function

--- arch/x86/boot/boot_sect.asm
+++ arch/x86/boot/boot_sect.asm
@@ -1,34 +1,12 @@
 [org 0x7c00]
 
-mov bx, HELLO_MSG
-call print_string
+mov dx, 0x1fb6
+call print_hex
 
-mov bx, LINE_FEED
-call print_string
 
-mov bx, CARRIAGE_RETURN
-call print_string
+jmp $
 
-mov bx, GOODBYE_MSG
-call print_string
-
-jmp $ 
-
-%include "print_string.asm"
-
-; Data
-
-HELLO_MSG:
-    db 'Hello world!', 0
-
-GOODBYE_MSG:
-    db 'Goodbye!', 0
-
-LINE_FEED:
-    db 0x0a, 0
-
-CARRIAGE_RETURN:
-    db 0x0d, 0
+%include "print_hex.asm"
 
 ; Padding and magic number
 times 510-($-$$) db 0
