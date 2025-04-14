100fafe284869afb7bf42f4f6a349b948e6aa57e
leongit78 <leonidivanov993@gmail.com>
2025-04-14 04:44:35 +0300

    reading from the disk implementation Added

--- arch/x86/boot/boot_sect.asm
+++ arch/x86/boot/boot_sect.asm
@@ -1,16 +1,33 @@
-mov ah, 0x0e
+[org 0x7c00]
 
-mov bx, 0x7c0; 0x7c0 * 16 + offset = [org 0x7c00] => получен правильный абсолютный адрес символа => он выводится на экран 
-mov ds, bx ; Дополнительный шаг, так как в сегментные регистры нельзя напрямую класть значения адресов
-mov al, [the_secret]
-int 0x10
+    mov [BOOT_DRIVE], dl
 
-jmp $
+    mov bp, 0x8000
+    mov sp, bp
 
-the_secret:
-    db "X"
+    mov bx, 0x9000
+    mov dh, 5
+    mov dl, [BOOT_DRIVE]
+    call disk_load
 
-; Padding and magic number
+    mov dx, [0x9000]
+    call print_hex
+
+    call print_nl
+
+    mov dx, [0x9000 + 512]
+    call print_hex
+
+    jmp $
+
+%include "functions/print/print_string.asm"
+%include "functions/print/print_hex.asm"
+%include "functions/disk/disk_load.asm"
+
+BOOT_DRIVE: db 0
+
 times 510-($-$$) db 0
-
 dw 0xaa55
+
+times 256 dw 0xdada
+times 256 dw 0xface
